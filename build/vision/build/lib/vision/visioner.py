3f95ca55e26f3f5af0b605e6e479b4e1af682c5f
Kak-Palel <farrelganen@gmail.com>
2024-02-23 21:14:36 +0700

    added comments

--- build/vision/build/lib/vision/visioner.py
+++ build/vision/build/lib/vision/visioner.py
@@ -4,102 +4,123 @@ from std_msgs.msg import String
 import cv2 as cv
 import numpy as np
 
-
-
+"""
+Class untuk menghandle penglihatan bot dan juga menggambar gerakan bot
+"""
 class VisionerTicTacToe(Node):
 
     def __init__(self):
         super().__init__('VisionerTicTacToe')
 
-        cv.namedWindow("ni olel")
-        cv.createTrackbar('param1', 'ni olel', 100, 200, self.nothing)
-        cv.createTrackbar('param2', 'ni olel', 15, 200, self.nothing)
+        cv.namedWindow("ni olel")                                           #window dibutuhkan supaya dapat menggunakan trackbar
+        cv.createTrackbar('param1', 'ni olel', 100, 200, self.nothing)      #trackbar untuk kalibrasi threshold yang di pass ke canny edge detector
+        cv.createTrackbar('param2', 'ni olel', 15, 200, self.nothing)       #trackbar untuk kalibrasi akurasi deteksi lingkaran terhadap kualitas cahaya
+        
+        #buka camera
         self.cap = cv.VideoCapture(0)
         if not self.cap.isOpened():
             print("HUAAAAAAAAAA RUSAAAAAAAAAAAK")
-
+        
+        #array untuk menyimpan grid mana saja yang ditempati oleh bot
         self.botMoves = np.array([])
         
+        #membuat publisher dan memanggil method 'timer_callback()' 30 kali sedetik
         self.publisher_ = self.create_publisher(String, 'ttt_state', 10)
         timer_period = 0.032  # seconds | 30 fps
         self.timer = self.create_timer(timer_period, self.timer_callback)
 
+        #membuat subscription untuk menerima perintah gerakan bot
         self.subscription = self.create_subscription(
             String,
             'ttt_moves',
             self.listener_callback,
             10)
-        self.subscription
+        self.subscription #ini biar ga di warning bjir
+
         print("constructor excuted")
 
+    #dummy method sebagai callback function dari trackbar
     def nothing(self):
         pass
     
+    """
+    Method untuk membaca keadaan grid.
+    karena method ini terpanggil 30 kali perdetik, maka method ini dapat membaca kamera, memproses, dan 
+    menampilkannya ke layar. Hasil proses diubah kebentuk string agar lebih mudah untuk dikirim. Method
+    ini me-return string yang telah di proses tadi apabila user menekan tombol "x" yang menandakan pergantian
+    giliran.
+    """
     def readState(self):
-        
+        #membaca frame kamera
         successOpenCam, drawnFrame = self.cap.read()
-
         if not successOpenCam:
             print("NDAK BISA BACA HUAAA")
             return
         
+        #buat copy dari drawnFrame dengan nama processFrame yang akan kita gunakan untuk memproses
         processFrame = drawnFrame.copy()
         
+        #gambar kotak untuk memudahkan user menemukan dimana seharusnya grid tictactoe diletakkan
         drawnFrame = cv.rectangle(drawnFrame, (150, 110), (490, 450), (255, 0, 0), 2)
         
+        #menutupi area yang tidak perlu untuk menghindari deteksi lingkaran yang tidak diinginkan
         processFrame = cv.rectangle(processFrame, (0, 0), (150, 480), (0, 0, 0), -1)
         processFrame = cv.rectangle(processFrame, (150, 0), (490, 110), (0, 0, 0), -1)
         processFrame = cv.rectangle(processFrame, (490, 0), (640, 480), (0, 0, 0), -1)
         processFrame = cv.rectangle(processFrame, (150, 450), (490, 480), (0, 0, 0), -1)
+
+        #convert ke hsv dan apply blur
         processFrame = cv.cvtColor(processFrame, cv.COLOR_BGR2HSV)
-
-        # processFrame = cv.GaussianBlur(processFrame, (5, 5), 2, 2)
-        # processFrame = cv.blur(processFrame, (5,5))
         processFrame = cv.medianBlur(processFrame, 5)
-        # processFrame = cv.bilateralFilter(processFrame, 15, 100, 100)
 
+        #buat dua foto copy yang nanti digabung karena merah memiliki 2 interval hsv
         mask1 = processFrame.copy()
         mask2 = processFrame.copy()
         
+        #mask untuk nilai hsv merah yang pertama
         upper_range = np.array([15, 255, 255])
         lower_range = np.array([0, 100, 100])
-
         mask1 = cv.inRange(mask1, lower_range, upper_range)
 
+        #mask untuk nilai hsv merah yang kedua
         upper_range = np.array([179, 255, 255])
         lower_range = np.array([155, 100, 100])
-
         mask2 = cv.inRange(mask2, lower_range, upper_range)
 
+        #gabungkan kedua mask untuk mendapatkan semua warna merah
         processFrame = mask1 + mask2
         
-        pram1 = int(cv.getTrackbarPos('param1', 'ni olel'))
-        pram2 = int(cv.getTrackbarPos('param2', 'ni olel'))
+        pram1 = int(cv.getTrackbarPos('param1', 'ni olel')) #upper threshold yang di pass ke canny edge detector, sebagai parameter yang dapat diubah dari trackbar
+        pram2 = int(cv.getTrackbarPos('param2', 'ni olel')) #tingkat akurasi deteksi lingkaran tergantung pada kualitas cahaya, sebagai parameter yang dapat diubah dari trackbar
 
-        # circles = cv.HoughCircles(processFrame, cv.HOUGH_GRADIENT, 1.4, 50)
+        #deteksi lingkaran
         circles = cv.HoughCircles(processFrame, cv.HOUGH_GRADIENT, 1, 60, param1 = pram1, param2 = pram2, minRadius = 40, maxRadius = 0)
             
+        #batas batas tiap kolom di grid dan juga index kolomnya
         gridPos = np.array([
-            [150, 263, 110, 223, 1],
-            [264, 376, 110, 223, 2],
-            [377, 490, 110, 223, 3],
-            [150, 263, 224, 336, 4],
-            [264, 376, 224, 336, 5],
-            [377, 490, 224, 336, 6],
-            [150, 263, 337, 450, 7],
-            [264, 376, 337, 450, 8],
-            [377, 490, 337, 450, 9]])
+            [150, 263, 110, 223, 0],
+            [264, 376, 110, 223, 1],
+            [377, 490, 110, 223, 2],
+            [150, 263, 224, 336, 3],
+            [264, 376, 224, 336, 4],
+            [377, 490, 224, 336, 5],
+            [150, 263, 337, 450, 6],
+            [264, 376, 337, 450, 7],
+            [377, 490, 337, 450, 8]])
         
+        #kondisi grid dalam bentuk array
         gridState = np.array([0, 0, 0, 0, 0, 0 ,0, 0, 0])
 
+        #gambar posisi bidak player yang terdeteksi dan assign sebagai gerakan yang telah dibuat oleh player
         if circles is not None:
             circlesInt = np.round(circles[0, :]).astype("int")
             for (x, y, r) in circlesInt:
                 cv.circle(drawnFrame, (x, y), r, (255, 0, 0), 2)
                 for i in gridPos:
-                    if i[0] <= x and x <= i[1] and i[2] <= y and y <= i[3] and gridState[i[4]-1] != 2:
-                        gridState[i[4]-1] = 1
+                    if i[0] <= x and x <= i[1] and i[2] <= y and y <= i[3] and gridState[i[4]] != 2:
+                        gridState[i[4]] = 1
         
+        #gambar posisi bidak bot dan assign sebagai gerakan yang telah dibuat oleh bot
         if self.botMoves is not None:
             for i in self.botMoves:
                 i = int(i)
@@ -111,22 +132,22 @@ class VisionerTicTacToe(Node):
                 elif 6 <= i <= 8:
                     cv.circle(drawnFrame, (150 + 56*(2*(i-6)+1), 390), 50, (255, 0, 0), -1)
 
-
+        #tunjukkan gambar utama dan juga gambar yang dilihat oleh komputer untuk keperluan kalibrasi
         cv.imshow("olel ngeri", processFrame)
         cv.imshow("ni olel", drawnFrame)
         
+        #konversi kondisi grid dari bentuk array ke string
         msg = ''
         for i in gridState:
             msg += str(i)
 
-        if cv.waitKey(1) == ord('x'):
+        if cv.waitKey(1) == ord('x'):       #giliran selanjutnya
             return msg
-        else:
-            return 'zonk'
-
-    def timer_callback(self):
         
+        return 'zonk'
 
+    #method publisher yang hanya mempublish apabila user telah memberi aba aba untuk berpindah gantian
+    def timer_callback(self):
         msg = String()
         msg.data = self.readState()
         if msg.data != 'zonk':
@@ -134,11 +155,12 @@ class VisionerTicTacToe(Node):
             print(msg.data)
             print('move sent, processing...')
 
+    #method listener yang menerima perintah gerakan yang telah dikirim oleh node control dan menyimpannya
     def listener_callback(self, msg):
         self.get_logger().info('bot move to grid "%s"' % msg.data)
         self.botMoves =  np.append(self.botMoves, int(msg.data))
         
-
+#fungsi main (duh)
 def main(args=None):
     rclpy.init(args=args)
 
@@ -148,7 +170,6 @@ def main(args=None):
 
     visioner.destroy_node()
     rclpy.shutdown()
-
 
 if __name__ == '__main__':
     main()
--- src/control/src/processor.cpp
+++ src/control/src/processor.cpp
@@ -10,15 +10,27 @@
 using namespace std::chrono_literals;
 using std::placeholders::_1;
 
+/**
+ * Class untuk menerima keadaan grid, menentukan gerakan terbaik, dan mengirimkannya.
+ * Menggunakan algoritma alphabeta yang merupakan ekstensi yang lebih efisien dari
+ * algoritma minimax, suatu algoritma yang sering digunakan dalam game game yang jelas,
+ * bekerja dengan cara mengiterasi semua gerakan yang dapat diambil dan secara rekursif
+ * melihat semua probabilitas keadaan grid yang dapat terjadi untuk menentukan gerakan
+ * mana yang terebaik
+ * Sumber: "https://cwoebker.com/posts/tic-tac-toe"
+*/
 class ControllerTicTacToe : public rclcpp::Node
 {
   public:
+    //yang publik constructornya doang aowkaowokaokwo
     ControllerTicTacToe()
     : Node("bot_control"), count_(0)
     {
+      //inisiasi subscription untuk menerima kondisi grid dari node vision
       subscription_ = this->create_subscription<std_msgs::msg::String>(
       "ttt_state", 10, std::bind(&ControllerTicTacToe::topic_callback, this, _1));
-    
+
+      //inisiasi publisher untuk mengirim perintah gerakan yang telah ditentukan
       publisher_ = this->create_publisher<std_msgs::msg::String>("ttt_moves", 10);
       timer_ = this->create_wall_timer(
       500ms, std::bind(&ControllerTicTacToe::timer_callback, this));
@@ -26,25 +38,38 @@ class ControllerTicTacToe : public rclcpp::Node
     }
 
   private:
+    //default value dari gerakan yang akan dikirim, jika nilainya berubah, maka perhitungan sudah selesai dan siap dikirim
     int finalMove = -1;
     
+    /**
+     * method listener yang menerima kondisi grid, memanggil fungsi yangn mengubahnya ke bentuk 
+     * vector dan fungsi yang bertugas untuk menentukan gerakan, dan lalu menyimpannya pada finalMove untuk kemudian dikirim
+     * */
     void topic_callback(const std_msgs::msg::String::SharedPtr msg)
     {
+      //terima posisi grid
       RCLCPP_INFO(this->get_logger(), "A: '%s'", msg->data.c_str());
       std::string strBoard = msg->data.c_str();
       std::cout<<strBoard<<std::endl;
+      
+      //ubah dalam bentuk vector
       std::vector<int> vecBoard = this->string2vec(strBoard);
+
+      //print untuk keperluan debugging
       for(int j = 0; j < 9; j++)
       {
         std::cout<<vecBoard[j]<<" ";
         if((j+1) % 3 == 0){std::cout<<std::endl;}
       }
       std::cout<<std::endl;
+
+      //panggil fungsi determine dan simpan ke finalMove
       finalMove = determine(vecBoard);
       std::cout<<"move: "<<finalMove<<std::endl;
     }
     rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
 
+    //method yang mempublish finalMove dalam bentuk string apabila nilainya berubah dari default valuenya (-1)
     void timer_callback()
     {
       if(finalMove != -1)
@@ -59,6 +84,7 @@ class ControllerTicTacToe : public rclcpp::Node
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
     size_t count_;
 
+    //method untuk mengubah string posisi grid ke vector
     std::vector<int> string2vec(std::string strBoard) const
     {
       std::vector<int> vecBoard;
@@ -69,6 +95,7 @@ class ControllerTicTacToe : public rclcpp::Node
       return vecBoard;
     }
     
+    //kombinasi kombinasi kolom grid yang menandakan kemenangan bila di populasi bersamaan
     std::vector<std::vector<int>> winCombination =
     {
       {0, 1, 2},
@@ -81,9 +108,9 @@ class ControllerTicTacToe : public rclcpp::Node
       {2, 4, 6}
     };
 
+    //method untuk mengecek suatu player telah menang atau belum
     bool checkWin(std::vector<int> boardState, int player)
     {
-      //loop through all the win combo, if any i for all j in boardState[winCombination[i][j]] == player, return true
       for (uint64_t i = 0; i < winCombination.size(); i++)
       {
         if
@@ -97,6 +124,7 @@ class ControllerTicTacToe : public rclcpp::Node
       return false;
     }
 
+    //method untuk menentukan kolom grid mana saja yang dapat menjadi gerakan bot
     std::vector<int> availableMoves(std::vector<int> boardState)
     {
       std::vector<int> moves;
@@ -107,6 +135,7 @@ class ControllerTicTacToe : public rclcpp::Node
       return moves;
     }
 
+    //method implementasi algoritma alpha-beta
     int alphaBeta(std::vector<int> boardState, int playing, int alpha, int beta)
     {
       if(checkWin(boardState, 2)) {return 1;}
@@ -120,7 +149,6 @@ class ControllerTicTacToe : public rclcpp::Node
         }
       }
 
-      //else loop thru all moves that we can make, and then do a recursive search with alternating Player
       int score = 0;
       std::vector<int> emptyGrids = availableMoves(boardState);
       for(uint64_t i = 0; i < emptyGrids.size(); i++)
@@ -142,6 +170,7 @@ class ControllerTicTacToe : public rclcpp::Node
       if(playing == 2) {return alpha;} return beta;
     }
 
+    //method yang menentukan gerakan terbaik dengan menerapkan algoritma alpha-beta pada semua gerakan yang mungkin
     int determine(std::vector<int> boardState)
     {
       int maxScore = -2;
@@ -165,6 +194,7 @@ class ControllerTicTacToe : public rclcpp::Node
     }
 };
 
+//entry
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
